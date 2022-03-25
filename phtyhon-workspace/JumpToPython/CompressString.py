f1395773070c334a8b75a6e4f202371361f5080b
DESKTOP-V7PGB3V\jihun <jihun6508@gmail.com>
2022-03-25 18:33:58 +0900

    점프투 파이썬 무자열 압축하기(문제 미해결)

---
+++ phtyhon-workspace/JumpToPython/CompressString.py
@@ -0,0 +1,17 @@
+#점프 투 파이썬 종합문제 15. 문자열 압축하기
+
+InputStr = input()
+ResultStr = ""
+count = 1
+for i in range(len(InputStr)):
+    try:
+        if InputStr[i] == InputStr[i+1]:
+            count +=1
+        else : 
+            ResultStr = ResultStr + InputStr[i] + str(count)
+            count = 1
+    except IndexError:
+        pass
+
+print(ResultStr)
+         
